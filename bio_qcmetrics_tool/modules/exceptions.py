4c15c5a8d51bafab72d3334ad386314db60110bd
Kyle Hernandez <khernandez@bsd.uchicago.edu>
2018-05-23 15:37:49 +0000

    feat(error): add exceptions

---
+++ bio_qcmetrics_tool/modules/exceptions.py
@@ -0,0 +1,29 @@
+"""Module containing exceptions raised by QC tools"""
+
+class BioQcMetricsException(Exception):
+    """Base for all custom exceptions"""
+    pass
+
+class ParserException(BioQcMetricsException):
+    """Exception thrown when a QC tool can't parse a file"""
+    def __init__(self, message):
+        self.message = message 
+
+    def __str__(self):
+        return self.message
+
+class UnsupportedTypeException(BioQcMetricsException):
+    """Exception thrown when tool encounters an unsupported type"""
+    def __init__(self, message):
+        self.message = message 
+
+    def __str__(self):
+        return self.message
+
+class ClassNotFoundException(BioQcMetricsException):
+    """Exception thrown when tool is unable to locate a class""" 
+    def __init__(self, message):
+        self.message = message 
+
+    def __str__(self):
+        return self.message
