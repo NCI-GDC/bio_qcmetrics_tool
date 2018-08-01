9256c7bc4b4f82fe1d473447bd738283949aa277
Kyle Hernandez <khernandez@bsd.uchicago.edu>
2018-08-01 01:00:43 +0000

    feat(samtools): stats

--- bio_qcmetrics_tool/modules/samtools/__init__.py
+++ bio_qcmetrics_tool/modules/samtools/__init__.py
@@ -2,3 +2,4 @@ from __future__ import absolute_import
 
 from .export_flagstats import ExportSamtoolsFlagstats 
 from .export_idxstats import ExportSamtoolsIdxstats 
+from .export_stats import ExportSamtoolsStats
