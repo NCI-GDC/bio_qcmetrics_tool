1aac43c8946e225ad5889010f1d97883d23da80a
Kyle Hernandez <khernandez@bsd.uchicago.edu>
2020-03-10 18:09:49 +0000

    feat(tests): add more utils

--- tests/utils.py
+++ tests/utils.py
@@ -1,5 +1,5 @@
 import sys
-
+import os
 from io import StringIO
 from contextlib import contextmanager
 
@@ -17,3 +17,40 @@ def captured_output():
         yield sys.stdout, sys.stderr
     finally:
         sys.stdout, sys.stderr = old_out, old_err
+
+
+def get_test_data_path(fname):
+    """
+    Helper function to get full path to a test
+    dataset give the basename.
+    :param fname: test dataset file basename
+    """
+    path = os.path.join(os.path.dirname(__file__), "data/{0}".format(fname))
+    return path
+
+
+def get_table_list(cur):
+    """
+    Gets a list of tables in the sqlite db provided the cursor.
+    """
+    res = cur.execute("SELECT name FROM sqlite_master WHERE type='table'")
+    rows = list([i[0] for i in list(res.fetchall())])
+    return rows
+
+
+def cleanup_files(files):
+    """
+    Takes a file or a list of files and removes them.
+    """
+    def _do_remove(fil):
+        if os.path.exists(fil):
+            os.remove(fil)
+ 
+    flist = []
+    if isinstance(files, list):
+        flist = files[:] 
+    else:
+        flist = [files]
+
+    for fil in flist:
+        _do_remove(fil)
