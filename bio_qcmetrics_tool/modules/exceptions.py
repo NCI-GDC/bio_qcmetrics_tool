705329159c9e970a671a808797aa5e2fdffa1d4f
Charles Czysz <czysz@uchicago.edu>
2022-02-28 10:11:19 -0600

    Simplify custom exceptions

--- bio_qcmetrics_tool/modules/exceptions.py
+++ bio_qcmetrics_tool/modules/exceptions.py
@@ -4,44 +4,32 @@
 class BioQcMetricsException(Exception):
     """Base for all custom exceptions"""
 
-    pass
+    def __init__(self, message: str):
+        self.message = message
+
+    def __str__(self) -> str:
+        return self.message
 
 
 class ParserException(BioQcMetricsException):
     """Exception thrown when a QC tool can't parse a file"""
 
-    def __init__(self, message):
-        self.message = message
-
-    def __str__(self):
-        return self.message
+    pass
 
 
 class UnsupportedTypeException(BioQcMetricsException):
     """Exception thrown when tool encounters an unsupported type"""
 
-    def __init__(self, message):
-        self.message = message
-
-    def __str__(self):
-        return self.message
+    pass
 
 
 class ClassNotFoundException(BioQcMetricsException):
     """Exception thrown when tool is unable to locate a class"""
 
-    def __init__(self, message):
-        self.message = message
-
-    def __str__(self):
-        return self.message
+    pass
 
 
 class DuplicateInputException(BioQcMetricsException):
     """Exception thrown when a QC tool encounters duplicate inputs"""
 
-    def __init__(self, message):
-        self.message = message
-
-    def __str__(self):
-        return self.message
+    pass
